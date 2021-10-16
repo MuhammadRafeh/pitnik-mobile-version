cb743361bb55434f7d3c713d6285f6d2a06efec5
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-10-16 16:08:20 +0500

    Added initial files!

---
+++ components/Post.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import propTypes from 'prop-types';
+
+const Post = props => { //name //dp // time// title// 
+    return (
+        
+    );
+}
+
+export default Post;
+
+const styles = StyleSheet.create({
+
+});
+
+
