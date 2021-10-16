156ef192e509748e507b698587af1e0db618c02a
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-10-16 17:19:03 +0500

    Added Post

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -25,8 +25,9 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Post from './components/Post';
 
-const Section = ({children, title}): Node => {
+const Section = ({ children, title }): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -65,26 +66,7 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <Post isDarkMode={isDarkMode} />
       </ScrollView>
     </SafeAreaView>
   );
--- components/Post.js
+++ components/Post.js
@@ -1,17 +1,128 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import propTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/Entypo';
 
-const Post = props => { //name //dp // time// title// 
+const Post = props => { //name //dp // time// postTitle// 
     return (
-        
+        <View style={[styles.container, { color: props.isDarkMode ? 'white' : 'black' }]}>
+
+            {/* Header */}
+            <View style={styles.header}>
+                <View style={styles.section1}>
+
+                    <View style={styles.dpContainer}>
+                        <Image source={{ uri: props.dp }} style={{ width: 30, height: 30 }} />
+                    </View>
+                    <View>
+                        <View>
+                            <Text>Saim Taren</Text>
+                        </View>
+                        <View>
+                            <Text>1 weeks ago</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.section2}>
+                    <Icon name={'dots-three-horizontal'} size={18} color={props.isDarkMode ? 'white' : 'black'}/>
+                </View>
+
+            </View>
+
+            {/* Mid */}
+            <View style={styles.post}>
+                <View style={styles.postTitle}>
+                    <Text numberOfLines={3} adjustsFontSizeToFit={true}>This is a post title  asdasd as d description and i am muhammad rafeh and i am doing task at this time</Text>
+                </View>
+
+                <View style={styles.postImage}>
+                    <Image source={{ uri: props.uri }} />
+                </View>
+            </View>
+
+            {/* footer */}
+            <View style={styles.footer}>
+                <View style={styles.action}>
+                    <Text style={styles.actionText}>Like</Text>
+                </View>
+                <View style={styles.action}>
+                    <Text style={styles.actionText}>Comment</Text>
+                </View>
+                <View style={styles.action}>
+                    <Text style={styles.actionText}>Share</Text>
+                </View>
+            </View>
+
+        </View>
     );
 }
 
 export default Post;
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 400,
+        marginVertical: 20,
+        marginHorizontal: 10
+        // backgroundColor: 'green'
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 0.2
+    },
+    dpContainer: {
+        borderRadius: 30,
+        borderWidth: 1,
+        marginRight: 10
+    },
+    section1: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flex: 1
+    },
+    section2: {
+        flex: 1,
+        alignItems: 'flex-end',
+        marginRight: 20
+    },
+    post: {
+        flex: 1,
+        marginRight: 20
+    },
+    footer: {
+        flexDirection: 'row',
+        flex: 0.1,
+        alignItems: 'center'
+    },
+    action: {
+        flex: 1,
+        borderRightWidth: 1
+    },
+    actionText: {
+        textAlign: "center"
+    },
+    postTitle: {
+        flex: 0.15,
+        // backgroundColor: 'blue',
+        justifyContent: 'center',
 
+    },
+    postImage: {
+        flex: 1,
+        // backgroundColor: 'grey',
+
+    }
 });
 
-
+Post.propTypes = {
+    dp: propTypes.any,
+    name: propTypes.string,
+    data: propTypes.string,
+    postTitle: propTypes.string,
+    image: propTypes.any,
+    isDarkMode: propTypes.bool
+}
